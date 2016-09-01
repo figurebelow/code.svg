1f12eb63e11d4b57c9612554578a8744f414605e
figurebelow <ruben@figurebelow.com>
2016-09-01 10:34:55 +0100

    Added sync calls to colourlovers.com API

--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -5,12 +5,13 @@
 **/
 
 "use strict";
-let cl = require ("colourlovers");
+let request = require("sync-request");
 
 /**
  * @class
- * @classdesc This class provides a wrapper around colorlovers package to
- * retrieve palettes and its colors
+ * @classdesc This class retrieves Palette information from colourlovers.com.
+ * If you ever reuse this code remember all these calls are blocking, so unsuitable for
+ * a web server implementation.
  */
 class Colors {
 
@@ -18,35 +19,59 @@ class Colors {
     // static class
   };
 
-
-  static getRndColors () {
-    cl.get ("/palettes/random", {
-      // no params allowed
-    }, 
-    function (err, data) {
-      if (err)
-        throw err;
-      else {
-        console.log(data[0].id);
-        return (data[0].colors);
-      }
+  /**
+   * Returns a random palette
+   * @return {Object} returns a {id,colors} object containing the Palette Id and colors
+   */
+  static getRndPalette () {
+    var res = request ("GET", "http://www.colourlovers.com/api/palettes/random", {
+      qs: {"format":"json"}
     });
+    var jsonRes = JSON.parse(res.getBody('utf8'));
+    return jsonRes.map(function (i) { return {id:i.id, colors:i.colors }});
   };
 
-  static getTopColors (n) {
-    cl.get ("/palettes/new", {
-      numResults: (n || 20)
-    }, 
-    function (err, data) {
-      if (err)
-        throw err;
-      else {
-        console.log(data.map (function(elem) { 
-                      return {id:elem.id, colors:elem.colors} }));
-        return data;
-      }
+  /**
+   * Returns the n top palettes
+   * @parameter{number} n - number of palettes to retrieve
+   * @return {array} a list of {id,colors} objects containing the Palettes Id and colors
+   */
+  static getTopPalettes (n) {
+    var res = request ("GET", "http://www.colourlovers.com/api/palettes/top", {
+      qs: {"format":"json",
+           "numResults": n || 1
+          }
     });
+    var jsonRes = JSON.parse(res.getBody('utf8'));
+    return jsonRes.map(function (i) { return {id:i.id, colors:i.colors }});
+  };
 
+  /**
+   * Returns the n lates palettes added to colourlovers.com
+   * @parameter{number} n - number of palettes to retrieve
+   * @return {array} a list of {id,colors} objects containing the Palettes Id and colors
+   */
+  static getLatestPalettes (n) {
+    var res = request ("GET", "http://www.colourlovers.com/api/palettes/new", {
+      qs: {"format":"json",
+           "numResults": n || 1
+          }
+    });
+    var jsonRes = JSON.parse(res.getBody('utf8'));
+    return jsonRes.map(function (i) { return {id:i.id, colors:i.colors }});
+  };
+
+  /**
+   * Returns a palette given its Id
+   * @parameter{string} id - string Id 
+   * @return {array} a list of {id,colors} objects containing the Palettes Id and colors
+   */
+  static getPalette (id) {
+    var res = request ("GET", "http://www.colourlovers.com/api/palette/" + id, {
+      qs: {"format":"json"}
+    });
+    var jsonRes = JSON.parse(res.getBody('utf8'));
+    return {id: jsonRes[0].id, colors: jsonRes[0].colors};
   };
 };
 
